d95dbae44b4d28bd40ed7771be5e8ce613b07800
Corey Sawin <sawincp@gmail.com>
2023-11-07 18:05:09 -0500

    update model associations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+    belongs_to :user
+    belongs_to :dog_houses
+
+    validates :review, presence: {message: "Please enter a review"}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_many :dog_houses
+    has_many :reviews, through: :dog_houses
+
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true
+
 end
